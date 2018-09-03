2b936282267bedf255c9a3d7ce53b18ed23b745a
Sarah GP <sarah.groff.palermo@gmail.com>
2018-09-03 14:53:18 -0400

    init

---
+++ spec/language-la-habra-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageLaHabraView from '../lib/language-la-habra-view';
+
+describe('LanguageLaHabraView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
